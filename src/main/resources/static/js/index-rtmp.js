f837d93b6d7fb90cc912474476efcac0e046da84
ly <1142426899@qq.com>
2021-01-04 22:32:24 +0800

    还原 js

--- src/main/resources/static/js/index-rtmp.js
+++ src/main/resources/static/js/index-rtmp.js
@@ -1,14 +1,30 @@
+var baseUrl = 'http://123.57.131.21:10031';
+var sockerUrl = 'ws://123.57.131.21:10031/server';
+
 //滚动的dv
 var $dList = $('.div_scroll');
+
+initContainer();
+function initContainer() {
+    var container = document.getElementById("playercontainer1");
+    var container2 = document.getElementById("playercontainer2");
+    var container3 = document.getElementById("playercontainer3");
+
+    var containerHeight = container.offsetWidth * 4 / 6;
+    container.style.height = containerHeight+'px';
+    container2.style.height =  containerHeight+'px';
+    container3.style.height =  containerHeight+'px';
+
+}
 
 var websocket;
 // 首先判断是否 支持 WebSocket
 if ('WebSocket' in window) {
-    websocket = new WebSocket("ws://127.0.0.1:8000/server");
+    websocket = new WebSocket(sockerUrl);
 } else if ('MozWebSocket' in window) {
-    websocket = new MozWebSocket("ws://127.0.0.1:8000/server");
+    websocket = new MozWebSocket(sockerUrl);
 } else {
-    websocket = new SockJS("ws://127.0.0.1:8000/server");
+    websocket = new SockJS(sockerUrl);
 }
 
 websocket.onopen = function (evnt) {
@@ -21,14 +37,16 @@ websocket.onopen = function (evnt) {
 };
 //websocket 接受消息
 websocket.onmessage = function (evnt) {
-    if (evnt.data == "成功建立socket连接") {
-
+    if (evnt.data == "连接成功") {
+        console.log('websocket连接服务器成功。');
+        return;
     }
-    console.log('数据已接收:' + evnt.data.detect);
     var data = $.parseJSON(event.data);
+    // console.log('数据已接收:' + data.detect);
     if (data.detect != null && data.detect.length > 0) {
-        changeBottomImg(data);
         changeRealTimeViolation(data);
+        // changeBottomImg(data);
+        // drawArea(data);
     }
 };
 
@@ -45,21 +63,21 @@ function playVideo(num) {
         toastr.warning('播放地址不能为空!');
         return;
     }
-        document.getElementById("source_"+num).src = url;
-        document.getElementById("playercontainer"+num).load();
-        var player = new EZuikit.EZUIPlayer("playercontainer"+num);
-        // 日志
-        player.on('log', log);
+    document.getElementById("source_"+num).src = url;
+    document.getElementById("playercontainer"+num).load();
+    var player = new EZuikit.EZUIPlayer("playercontainer"+num);
+    // 日志
+    player.on('log', log);
 
-        function log(str){
+    function log(str){
 
-        }
+    }
 }
 //更改下面的图片
 function changeBottomImg(){
 
     $.ajax({
-        url: "http://127.0.0.1:8000/smartsafe/getBottomImage",
+        url: baseUrl + "/smartsafe/getBottomImage",
         type: 'get',
         async: false,
         success: function (data) {
@@ -82,48 +100,70 @@ function changeRealTimeViolation(data){
 function scrollDiv(data) {
     //获取div高度
     var length = $(".div_scroll").children().length;
-    if (length>=6){
-        var scrollHeight = $('.div_scroll div:first').height();
-        //滚出一个li的高度
-        $dList.stop().animate({marginTop:-scrollHeight},600,function () {
-            //动画结束后
-            var a=document.getElementsByClassName("div_scroll");
-            var b=document.getElementsByClassName("realTimeViolation");
-            //默认是从下标0开始，想要移除第二个就是a[1].remove();
-            b[0].remove();
-            $dList.css("marginTop",0);
-            var tempString = '';
-            if (data.detect!=null && data.detect.length>0){
-                for (var i=0;i<data.detect.length;i++){
-                    tempString =  data.detect.type+tempString;
-                }
+    if (data.detect != null && data.detect.length > 0)
+    {
+        for (var i = 0; i < data.detect.length; i++)
+        {
+            var tempDetect = data.detect[i];
+            var array = [];
+            array.push('<div class="realTimeViolation"><span>设备：');
+            array.push(data.deviceId);
+            array.push('，类型：');
+            array.push(getDetectTypeStr(tempDetect.type));
+            array.push('，概率：');
+            array.push(tempDetect.probability);
+            array.push('%，时间：');
+            array.push(new Date(tempDetect.time).Format('yyyy-MM-dd hh:mm:ss'));
+            array.push('。</span></div>');
+            if(length >= 13)
+            {
+                var scrollHeight = $('.div_scroll div:first').height();
+                //滚出一个li的高度
+                $dList.stop().animate({marginTop:-scrollHeight},100,function () {
+                    //动画结束后
+                    var b = document.getElementsByClassName('realTimeViolation');
+                    //默认是从下标0开始，想要移除第二个就是a[1].remove();
+                    // console.log('scrollHeight = ' + scrollHeight);
+                    b[0].remove();
+                    $dList.append(array.join(''));
+                    $dList.css('marginTop',0);
+                });
             }
-            $dList.append("<div class='realTimeViolation'><span>"+"设备 "+data.deviceId+"发生"+"</span><button class='btn btn-default btn_bottom' type='button'>进入</button></div>");
-        })
-    }else {
-        var tempString = '';
-        if (data.detect!=null && data.detect.length>0){
-            for (var i=0;i<data.detect.length;i++){
-                tempString = data.detect.type+tempString;
+            else
+            {
+                $dList.append(array.join(''));
+                length += 1;
             }
         }
-        $dList.append("<div class='realTimeViolation'><span>"+"设备 "+data.deviceId+"发生"+"</span><button class='btn btn-default btn_bottom' type='button'>进入</button></div>");
-
     }
+}
 
-
+function getDetectTypeStr(type){
+    if(type == 1)
+        return '安全帽';
+    if(type == 2)
+        return '安全绳';
+    if(type == 3)
+        return '着装';
+    if(type == 4)
+        return '跨越围栏';
+    return '未知';
 }
 
 //每隔5秒更新底部图片
 setInterval(changeBottomImg,5000);
 
-function timestampToTime(timestamp) {
-    var date = new Date(timestamp);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
-    var Y = date.getFullYear() + '-';
-    var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
-    var D = date.getDate() + ' ';
-    var h = date.getHours() + ':';
-    var m = date.getMinutes() + ':';
-    var s = date.getSeconds();
-    return Y+M+D+h+m+s;
+function getOSDTime(index){
+    var tempDecoder;
+    if (index == 1){
+        tempDecoder = decoder1;
+    }else if( index == 2){
+        tempDecoder = decoder2;
+    }else {
+        tempDecoder = decoder3;
+    }
+    var getOSDTimePromise = tempDecoder.getOSDTime();
+    getOSDTimePromise.then(function(data){
+        return data;
+    })
 }
